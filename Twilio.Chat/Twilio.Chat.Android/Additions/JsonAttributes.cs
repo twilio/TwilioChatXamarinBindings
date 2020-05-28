13d5e1d8fb43cb966cc7ffb85741532230ea4c56
Berkus Karchebnyy <berkus@twilio.com>
2020-05-28 13:45:22 +0300

    Implement JsonAttributes type accessors similar to iOS

---
+++ Twilio.Chat/Twilio.Chat.Android/Additions/JsonAttributes.cs
@@ -0,0 +1,12 @@
+﻿namespace Com.Twilio.Chat
+{
+    public partial class JsonAttributes : Java.Lang.Object
+    {
+        public bool IsNull => ValueType == AttributeType.Null;
+        public bool IsString => ValueType == AttributeType.String;
+        public bool IsNumber => ValueType == AttributeType.Number;
+        public bool IsArray => ValueType == AttributeType.Array;
+        public bool IsDictionary => ValueType == AttributeType.Object;
+        public bool IsBoolean => ValueType == AttributeType.Boolean;
+    }
+}
